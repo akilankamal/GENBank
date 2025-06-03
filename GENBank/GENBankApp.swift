059311bf32713c54bf43836751d2baa5b02cbcef
Akilan Kamalakannan <akilankamal@live.com>
2025-06-04 00:07:58 +0530

    Initial Commit

---
+++ GENBank/GENBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  GENBankApp.swift
+//  GENBank
+//
+//  Created by Akilan Kamalakannan on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GENBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GENBankTests/GENBankTests.swift
@@ -0,0 +1,17 @@
+//
+//  GENBankTests.swift
+//  GENBankTests
+//
+//  Created by Akilan Kamalakannan on 04/06/25.
+//
+
+import Testing
+@testable import GENBank
+
+struct GENBankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
